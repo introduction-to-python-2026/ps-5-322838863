e428792718e9ccc1ef4a106a0c7a0f239c773281
322838863 <shovalgos@gmail.com>
2025-11-21 21:38:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import string_utils as string_u , equation_utils as equation_u
+import string_utils as su, equation_utils as eu
 
-def balance_reaction(reaction):
-    """Balances a chemical reaction and returns the coefficients as rationals."""
-    reactants, products = equation_u.parse_chemical_reaction(reaction)
-    reactant_atoms = equation_u.count_atoms_in_reaction(reactants)
-    product_atoms = equation_u.count_atoms_in_reaction(products)
+def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
-    equations, coefficients = equation_u.build_equations(reactant_atoms, product_atoms)
-    solved = string_u.my_solve(equations, coefficients)
+    reactants, products = su.parse_chemical_reaction(reaction) 
+    reactant_atoms = su.count_atoms_in_reaction(reactants) 
+    product_atoms = su.count_atoms_in_reaction(products)
+    
+    equations, coefficients = eu.build_equations(reactant_atoms, product_atoms)
+    coefficients = eu.my_solve(equations, coefficients) + [1]
 
-    return solved + [1]  # last coefficient forced to 1
+    return coefficients 
