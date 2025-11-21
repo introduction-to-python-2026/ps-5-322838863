4e1cf6f7eab905ad93169aa4a6c1e6fc2c510443
322838863 <shovalgos@gmail.com>
2025-11-21 21:02:31 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,53 +1,36 @@
-
-
+# string_utils.py
 
 def split_before_uppercases(formula):
+    """Splits a string before every uppercase letter."""
     if not formula:
         return []
     parts = []
     current = formula[0]
-
     for ch in formula[1:]:
         if ch.isupper():
             parts.append(current)
             current = ch
         else:
             current += ch
-
     parts.append(current)
     return parts
-    
-     
+
 
 def split_at_digit(formula):
+    """Splits a string into element name and count (integer)."""
     for i, ch in enumerate(formula):
         if ch.isdigit():
-            # First digit found
-            prefix = formula[:i]
-            number = int(formula[i:])
-            return prefix, number
+            return formula[:i], int(formula[i:])
+    return formula, 1  # default count = 1
 
-    # No digit found
-    return formula, 1
-            
-    
+
 def count_atoms_in_molecule(formula):
+    """Returns a dictionary of atom counts for a single molecule."""
     atom_dict = {}
     for atom in split_before_uppercases(formula):
         atom_name, atom_count = split_at_digit(atom)
-        #atom_dict.update({atom_name: atom_count})
         atom_dict[atom_name] = atom_dict.get(atom_name, 0) + atom_count
     return atom_dict
 
 
-
-def parse_chemical_reaction(reaction_equation):
-    reaction_equation = reaction_equation.replace(" ", "")
-    reactants, products = reaction_equation.split("->")
-    return reactants.split("+"), products.split("+")
-
-def count_atoms_in_reaction(molecules_list):
-    molecules_atoms_count = []
-    for molecule in molecules_list:
-        molecules_atoms_count.append(count_atoms_in_molecule(molecule))
-    return molecules_atoms_count
+def parse_chemical_reacti
