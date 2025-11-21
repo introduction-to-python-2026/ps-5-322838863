1018863ea5f367a688f891189e39d90850bef1b1
322838863 <shovalgos@gmail.com>
2025-11-21 20:20:24 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -31,8 +31,8 @@ def split_at_digit(formula):
     return formula, 1
             
     
-atom_dict = {}
 def count_atoms_in_molecule(formula):
+    atom_dict = {}
     for atom in split_before_uppercases(formula):
         atom_name, atom_count = split_at_digit(atom)
         atom_dict.update({atom_name: atom_count})
